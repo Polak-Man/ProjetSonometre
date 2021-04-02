33713d59100102221a8d02d4c0cdcc4d58a3eaf6
kalde108 <kalde.chillon@hotmail.fr>
2021-04-02 11:46:36 +0200

    Add files via upload

--- Sauvegarde/Arduino/Sonometre/site.cpp
+++ Sauvegarde/Arduino/Sonometre/site.cpp
@@ -13,17 +13,24 @@ String header;
 // Set web server port number to 80
 WiFiServer server(80);
 
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
-const int output3 = 3;
+// Variable seuil volume
+int seuilVert = 0;
+int seuilOrange = 0;
+int seuilRouge = 0;
 
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output4State = "off";
-String output3State = "off";
+// Variable de récupération des seuil
+int debutV = 0;
+int debutO = 0;
+int debutR = 0;
+int finSeuil = 0;
+
+
+// Variable de sécurité
 String buttonAdmin = "off";
-String mdpAdmin = "";
+String mdpAdmin = "S0n0m%C3%A8tre";
+String mdpAdminInput = " ";
+int mdpStart = 0;
+int mdpEnd = 0;
 
 // Current time
 unsigned long currentTime = millis();
@@ -39,12 +46,6 @@ void parle() {
 }
 
 void WiFi_Setup() {
-  // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(output4, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(output4, LOW);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -87,7 +88,7 @@ void site() {
             client.println("Connection: close");
             client.println();
             
-            // turns the GPIOs on and off
+/*            // turns the GPIOs on and off
             if (header.indexOf("GET /5/on") >= 0) {
               Serial.println("GPIO 5 on");
               output5State = "on";
@@ -113,37 +114,36 @@ void site() {
               Serial.println("GPIO 3 off");
               output3State = "off";
               Serial.println("non");
-            }
+            }*/
 
             // Detection d'index
-            if (header.indexOf("GET /3/on") >= 0) {
+            if (header.indexOf("GET /5") >= 0) {
               Serial.println("Truc");
               
-      // page GPIO 3     
+      // page 2
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<head><meta charset=\"utf-8\" name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
             // CSS to style the on/off buttons 
             // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 { background-color: #77878A;}</style></head>");
+            client.println("<style> html { font-family: Helvetica; }");
+            client.println(".titre { position: relative; text-align: center; color: orange; }");
+            client.println(".center_button_case { position: relative; text-align: center; }");
+            client.println(".center_button {  background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; cursor: pointer; }");
+            client.println(".center_button2 { background-color: #77878A; }");
+            client.println(".up_right_button_case { display: inline-block; position: absolute; text-align: center; top: 5%; right: 5%; }");
+            client.println(".admin_button { position: relative; background-color: black; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; cursor: pointer; }");
+            client.println(".description { position: relative; }");
+            client.println("</style></head>");
             
             // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
+            client.println("<body><h1>5</h1>");
 
-          // GPIO 3  
-            // Display current state, and ON/OFF buttons for GPIO 3  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output3State=="on") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/3/on\"><button class=\"button button2\">ON</button></a></p>");
-            } 
-            
+          // Boutton Accueil  
+            // Display current state, and ON/OFF buttons for GPIO 5
+            client.println("<p><a href=\"/accueil\"><button class=\"center_button_case center_button\">Acceuil</button></a></p>");
+                        
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
@@ -151,6 +151,18 @@ void site() {
             // Break out of the while loop
             break;
             
+            } else if (header.indexOf("GET /?mdp=") >= 0) {
+              mdpStart = header.indexOf('=');
+              mdpEnd = header.indexOf('&');
+              mdpAdminInput = header.substring(mdpStart+1, mdpEnd);
+              Serial.println(mdpAdminInput);
+              if (mdpAdminInput == mdpAdmin) {
+                Serial.println("oui");
+              } else {
+                Serial.println("non");
+                client.println("https://youtu.be/dQw4w9WgXcQ");
+              }
+                            
             } else {
 
         // page accueil
@@ -160,18 +172,36 @@ void site() {
             client.println("<link rel=\"icon\" href=\"data:,\">");
             // CSS to style the on/off buttons 
             // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block;}");
-            client.println(".titre { align-self: center; color: orange;}");
-            client.println(".left_button { left: 5%; background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".left_button2 {background-color: #77878A;}");
+            client.println("<style> html { font-family: Helvetica; }");
+            client.println(".titre { position: relative; top: 3%; text-align: center; color: orange; font-size: 50px; }");
+            client.println(".center_button_case { position: relative; top: 10%; text-align: center; }");
+            client.println(".center_button {  background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; cursor: pointer; }");
+            client.println(".center_button2 { background-color: #77878A; }");
+            client.println(".up_right_button_case { display: inline-block; position: absolute; text-align: center; top: 5%; right: 5%; }");
+            client.println(".desc_parametre { position: relative; font-size: 20px; font-weight: bold; margin-block-end: 5px;}");
+            client.println(".admin_button { position: relative; background-color: #e6e6e6; border: none; color: black; padding: 6px 10px; text-decoration: none; font-size: 20px; cursor: pointer; }");
+            client.println(".mdp_input { position: relative; }");
+            client.println(".description { position: relative; }");
             client.println("</style></head>");
-            
-            //client.println("");
             
             // Web Page Heading
             client.println("<body><h1 class=\"titre\">Sonomètre</h1>");
-            
+
+          // Boutton 5
+            // Afficher le boutton
+            client.println("<div class=\"center_button_case\">");
+            client.println("<p class=\"description\">Page 2</p>");
+            client.println("<p><a href=\"/5\"><button class=center_button>ON</button></a></p>");
+            client.println("</div>");
+
+          // Formulaire Connexion
+            client.println("<form class=\"up_right_button_case\">");
+            client.println("<p class=\"desc_parametre\">Paramètre</p>");
+            client.println("<input type=\"password\" class=\"mdp_input\" name=\"mdp\" placeholder=\"Mot de passe\"/> <br />");
+            client.println("<button type=\"submit\" class=\"admin_button\" name=\"envoyer\">Acceder</button>");
+            client.println("</form>");
+
+          /*
             // Display current state, and ON/OFF buttons for GPIO 5  
             client.println("<p>GPIO 5 - State " + output5State + "</p>");
             // If the output5State is off, it displays the ON button       
@@ -180,7 +210,8 @@ void site() {
             } else {
               client.println("<p><a href=\"/5/off\"><button class=\"left_button left_button2\">OFF</button></a></p>");
             } 
-               
+
+          
             // Display current state, and ON/OFF buttons for GPIO 4  
             client.println("<p>GPIO 4 - State " + output4State + "</p>");
             // If the output4State is off, it displays the ON button       
@@ -189,6 +220,7 @@ void site() {
             } else {
               client.println("<p><a href=\"/4/off\"><button class=\"left_button left_button2\">OFF</button></a></p>");
             }
+          
 
             // Display current state, and ON/OFF buttons for GPIO 3  
             client.println("<p>GPIO 3 - State " + output3State + "</p>");
@@ -198,7 +230,7 @@ void site() {
             } else {
               client.println("<p><a href=\"/3/off\"><button class=\"left_button left_button2\">OFF</button></a></p>");
             } 
-            
+          */
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
